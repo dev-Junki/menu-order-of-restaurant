3312e1e2392bcc94a1203a041d722a7792f6bb42
Junki Tabira <infoxjunki@icloud.com>
2020-06-06 19:14:30 +0900

    add indent.

--- menu_item.py
+++ menu_item.py
@@ -1,7 +1,7 @@
 class MenuItem:
     def __init__(self, name, price):
-    self.name = name
-    self.price = price
+        self.name = name
+        self.price = price
     
     
     def info(self):
